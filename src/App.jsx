4a9dcb4579afd84dc718c08ea00a8bae3b316625
ShaikAyub7 <skayub929@gmail.com>
2025-01-27 18:18:59 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import About from "./components/About.jsx";
 import Home from "./components/Home-page.jsx";
 import Navbar from "./components/Navbar.jsx";
-import Projects from "./components/projects.jsx";
+import Projects from "./components/Projects.jsx";
 import Skills from "./components/Skills.jsx";
 
 import { skillList } from "./data.jsx";

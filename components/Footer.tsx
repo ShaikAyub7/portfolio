fd7eacce42e3fd2550170fcd3078d2a27dc0ad4e
ShaikAyub7 <skayub929@gmail.com>
2026-04-11 22:04:11 +0530

    fix: remove redundant text from copyright notice in Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-blue-950 border-t p-4 flex items-center justify-center text-gray-400 ">
-      copyright &copy; {new Date().getFullYear()} ShaikAyub - All Right Reserved
+       &copy; {new Date().getFullYear()} ShaikAyub - All Right Reserved
     </div>
   );
 };

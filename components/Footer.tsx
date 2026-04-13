bd63ec84a2d6958afa0b530cb817361b2200cbf7
ShaikAyub7 <skayub929@gmail.com>
2026-04-13 16:40:13 +0530

    fix: change div to footer element in Footer component for semantic HTML

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div className="bg-blue-950 border-t p-4 flex items-center justify-center text-gray-400 ">
-       &copy; {new Date().getFullYear()} ShaikAyub - All Right Reserved
-    </div>
+    <footer className="bg-blue-950 border-t p-4 flex items-center justify-center text-gray-400 ">
+      &copy; {new Date().getFullYear()} ShaikAyub - All Right Reserved
+    </footer>
   );
 };
 

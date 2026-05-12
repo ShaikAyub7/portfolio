cc9e4e20c454c0f704e5d339c88f911a06b35602
ShaikAyub7 <skayub929@gmail.com>
2026-05-12 14:27:13 +0530

    fix: remove unnecessary blank lines in Projects component for cleaner code

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { projects } from "@/data";
 import { FaGithub } from "react-icons/fa";
 import { LiaExternalLinkAltSolid } from "react-icons/lia";
@@ -14,7 +13,6 @@ export default function Projects() {
         whileInView={{ opacity: 1, y: 0 }}
       >
         <h2 className="text-3xl font-bold text-center mb-12">Projects</h2>
-
         <div className="flex flex-col gap-6">
           {projects.map((project) => (
             <div
